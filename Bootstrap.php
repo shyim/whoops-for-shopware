05b1832625840622f05a6c4cb0c3efcf11e890c7
Shyim <shyim@outlook.de>
2015-12-03 10:06:21 +0100

    Removed Whoops as Vendor

--- Bootstrap.php
+++ Bootstrap.php
@@ -1,50 +1,51 @@
 <?php
 class Shopware_Plugins_Core_WhoopsForShopware_Bootstrap extends Shopware_Components_Plugin_Bootstrap {
-	public function getLabel() {
-		return 'Whoops for Shopware';
-	} // END public function getLabel()
+    public function getLabel() {
+        return 'Whoops for Shopware';
+    }
 
-	public function getInfo() {
-		return array(
-			'version' => '1.0.0',
-			'autor' => 'Shyim',
-			'label' => $this->getLabel(),
-			'support' => 'https://github.com/Shyim/whoops-for-shopware',
-			'link' => 'https://github.com/Shyim/whoops-for-shopware'
-		);
-	}
+    public function getInfo() {
+        return array(
+            'version' => '1.0.0',
+            'autor' => 'Shyim',
+            'label' => $this->getLabel(),
+            'support' => 'https://github.com/Shyim/whoops-for-shopware',
+            'link' => 'https://github.com/Shyim/whoops-for-shopware'
+        );
+    }
 
-	public function install() {
-		$this->subscribeEvent(
-			'Enlight_Controller_Front_StartDispatch',
-			'onShopwareStartDispatch'
-		);
-		return true;
-	}
+    public function install() {
+        $this->subscribeEvent(
+            'Enlight_Controller_Front_StartDispatch',
+            'onShopwareStartDispatch'
+        );
+        return true;
+    }
 
-	public function uninstall() {
-		return true;
-	}
+    public function uninstall() {
+        return true;
+    }
 
-	public function onShopwareStartDispatch(Enlight_Event_EventArgs $args) {
-		if($args->getSubject()->getParam('noErrorHandler')) {
-			if(strstr($_SERVER['REQUEST_URI'], '/backend')) {
-				return;
-			}
+    public function onShopwareStartDispatch(Enlight_Event_EventArgs $args) {
+        if($args->getSubject()->getParam('noErrorHandler')) {
+            if(strstr($_SERVER['REQUEST_URI'], '/backend')) {
+                return;
+            }
 
-			spl_autoload_register(array($this, 'loader'));
+            spl_autoload_register(array($this, 'loader'));
 
-			$whoops = new \Whoops\Run;
-			$whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
-			$whoops->register();
-		} // END if($args->getSubject()->getParam('noErrorHandler'))
-	} // END public function onShopwareStartDispatch()
+            $whoops = new \Whoops\Run;
+            $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+            $whoops->register();
+            restore_error_handler();
+        }
+    }
 
-	public function loader($className) {
-		if(!strstr($className, 'Whoops')) {
-			return;
-		}
-		$className = str_replace('\\','/',$className);
-		require_once($this->Path() . '/vendor/filp/' . $className . '.php');
-	} // END public function loader($className)
-} // END class Shopware_Plugins_Core_WhoopsForShopware_Bootstrap extends Shopware_Components_Plugin_Bootstrap
+    public function loader($className) {
+        if(!strstr($className, 'Whoops')) {
+            return;
+        }
+        $className = str_replace('\\','/',$className);
+        require_once($this->Path() . '/vendor/filp/' . $className . '.php');
+    }
+}
