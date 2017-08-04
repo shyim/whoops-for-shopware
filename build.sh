46937dea17521d62fe6a47fc23df046b87fb40de
Soner Sayakci <s.sayakci@gmail.com>
2017-08-04 14:14:28 +0200

    Fixed build script

--- build.sh
+++ build.sh
@@ -11,4 +11,7 @@ fi
 # Build new release
 mkdir -p WhoopsForShopware
 git archive ${commit} | tar -x -C WhoopsForShopware
+cd WhoopsForShopware
+composer install -o --no-dev
+cd ..
 zip -r WhoopsForShopware-${commit}.zip WhoopsForShopware
